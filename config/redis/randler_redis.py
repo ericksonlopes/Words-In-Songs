b9f5a6ea660ba064bc857e756064dc8e37ea3cfe
Erickson Lopes <ofc.erickson@gmail.com>
2023-02-26 13:44:40 -0300

    REFACTOR: ajustes nos nomes dos arquivos

--- config/redis/randler_redis.py
+++ config/redis/randler_redis.py
@@ -1,7 +1,7 @@
 import json
 from typing import List
 
-from config import ConnectRedis
+from config.redis.conect_redis import ConnectRedis
 from src.Exceptions import GetSentenceToJsonException, SetSentenceException
 from src.models import SetenceFound
 
